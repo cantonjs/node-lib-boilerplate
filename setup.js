8387957a5f6dbd64a5067be73589923fcb257932
3went2 <webb.png@gmail.com>
2018-06-14 12:41:54 +0800

    update setup.js

--- setup.js
+++ setup.js
@@ -1,52 +1,55 @@
-
-const fs = require('fs');
-const path = require('path');
-const childProcess = require('child_process');
-const readline = require('readline');
-const os = require('os');
-const pkg = require('./package.json');
+const fs = require("fs");
+const path = require("path");
+const childProcess = require("child_process");
+const readline = require("readline");
+const os = require("os");
+const pkg = require("./package.json");
 
 const rl = readline.createInterface({
 	input: process.stdin,
-	output: process.stdout,
+	output: process.stdout
 });
 
 const answers = {};
 
-const ask = (question, key, defaultValue) => new Promise((resolve) => {
-	const defaults = defaultValue || pkg[key];
-	rl.question(`${question} (default: ${defaults})\n`, (answer) => {
-		answers[key] = answer || defaults;
-		resolve();
+const ask = (question, key, defaultAnswer) =>
+	new Promise(resolve => {
+		const defaults = defaultAnswer || pkg[key];
+		rl.question(`${question} (default: ${defaults})\n`, answer => {
+			answers[key] = answer || defaults;
+			resolve();
+		});
 	});
-});
 
 const defaultName = path.basename(__dirname);
 
-ask('What is your project name?', 'name', defaultName)
-	.then(() => ask('What is description?', 'description', defaultName))
-	.then(() => ask('What is repository?', 'repository', `cantonjs/${defaultName}`))
-	.then(() => ask('Who is the author?', 'author'))
+ask("What is your project name?", "name", path.basename(__dirname))
+	.then(() => ask("What is description?", "description", answers.name))
+	.then(() =>
+		ask("What is repository?", "repository", `cantonjs/${answers.name}`)
+	)
+	.then(() => ask("Who is the author?", "author"))
 	.then(() => {
 		rl.close();
 		console.log(JSON.stringify(answers, null, 2));
 		Object.assign(pkg, answers);
 
-		const pkgFile = path.resolve(__dirname, 'package.json');
+		const pkgFile = path.resolve(__dirname, "package.json");
 		const newPkg = JSON.stringify(pkg, null, 2);
 		// console.log('\npackage.json:\n', newPkg);
-		fs.writeFileSync(pkgFile, newPkg, 'utf8');
+		fs.writeFileSync(pkgFile, newPkg, "utf8");
 
 		const isWindows = /^win32/.test(os.platform());
-		const rimrafCommand = isWindows ? 'rd /s /q' : 'rm -rf';
-		const gitDir = path.resolve(__dirname, '.git');
+		const rimrafCommand = isWindows ? "rd /s /q" : "rm -rf";
+		const gitDir = path.resolve(__dirname, ".git");
 		// console.log(`${rimrafCommand} ${gitDir}`);
 		childProcess.exec(`${rimrafCommand} ${gitDir}`);
 
-		console.log('Success.\n');
-		console.log('To install dependencies, please run `yarn` manually.\n');
+		console.log("Success.\n");
+		console.log("To install dependencies, please run `yarn` manually.\n");
 
 		fs.unlinkSync(__filename);
 	})
-	.catch((err) => { throw err; })
-;
+	.catch(err => {
+		throw err;
+	});
